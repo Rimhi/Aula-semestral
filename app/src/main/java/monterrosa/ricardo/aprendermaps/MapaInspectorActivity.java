c9e4870d223105ff4fa13e3d1dc72a27cbc61340
Ricardo Monterrosa Higuita <rimhi7@gmail.com>
2018-08-30 12:17:45 -0500

    Solucionado locationlistener

--- app/src/main/java/monterrosa/ricardo/aprendermaps/MapaInspectorActivity.java
+++ app/src/main/java/monterrosa/ricardo/aprendermaps/MapaInspectorActivity.java
@@ -1,16 +1,21 @@
 package monterrosa.ricardo.aprendermaps;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,7 +41,6 @@ import com.google.firebase.database.ValueEventListener;
 import java.text.DecimalFormat;
 
 public class MapaInspectorActivity extends AppCompatActivity implements OnMapReadyCallback{
-
     private GoogleMap mMap;
     private DatabaseReference baseDatos;
     private DatabaseReference trampaRef;
@@ -46,6 +50,8 @@ public class MapaInspectorActivity extends AppCompatActivity implements OnMapRea
     private static final double RADIO_TIERRA = 6378.137;
     private static final String TAG = "UtilidadesCoordenadas";
     private int numero = 1;
+    private LatLng userlocation = null;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,8 +67,31 @@ public class MapaInspectorActivity extends AppCompatActivity implements OnMapRea
         trampaRef = baseDatos.child("trampas");
         trampaRef.addChildEventListener(trampasHijoListener);
 
-    }
+       locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (userlocation ==null) {
+                    userlocation = new LatLng(location.getLatitude(), location.getLongitude());
+                }
+            }
 
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+    }
 
 
     @Override
@@ -75,22 +104,24 @@ public class MapaInspectorActivity extends AppCompatActivity implements OnMapRea
         mMap.getUiSettings().setMyLocationButtonEnabled(false);
         mMap.setMinZoomPreference(13.0f);
         mMap.setLatLngBoundsForCameraTarget(MONTERIA_CORDOBA_COLOMBIA);
-        LocationManager lm = (LocationManager)getSystemService(getApplicationContext().LOCATION_SERVICE);
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        final LatLng userlocation = new LatLng(location.getLatitude(),location.getLongitude());
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userlocation, 14));
+        LocationManager lm = (LocationManager) getSystemService(getApplicationContext().LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,1,locationListener);
+        if (userlocation!=null){mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userlocation, 14));}
+
+
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
-               // Toast.makeText(getApplicationContext(),""+calcularDistancia(userlocation,marker.getPosition()),Toast.LENGTH_LONG).show();
+                // Toast.makeText(getApplicationContext(),""+calcularDistancia(userlocation,marker.getPosition()),Toast.LENGTH_LONG).show();
 
                 return false;
             }
         });
+        final LatLng finalUserlocation = userlocation;
         mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
             @Override
             public void onInfoWindowClick(final Marker marker) {
-                if (calcularDistancia(userlocation,marker.getPosition())<=6){
+                if (calcularDistancia(finalUserlocation, marker.getPosition()) <= 6) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(MapaInspectorActivity.this);
                     builder.setTitle("¿Desea llenar formulario?")
                             .setNegativeButton("no", new DialogInterface.OnClickListener() {
@@ -101,21 +132,21 @@ public class MapaInspectorActivity extends AppCompatActivity implements OnMapRea
                             }).setPositiveButton("Si", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            final String [] idtrampa= marker.getTitle().split(" ");
-                            Intent intent = new Intent(MapaInspectorActivity.this,LlenarFormularioActivity.class);
-                            intent.putExtra("codigotrampa",idtrampa[1]);
+                            final String[] idtrampa = marker.getTitle().split(" ");
+                            Intent intent = new Intent(MapaInspectorActivity.this, LlenarFormularioActivity.class);
+                            intent.putExtra("codigotrampa", idtrampa[1]);
                             startActivity(intent);
 
                         }
                     });
-                   dialog = builder.create();
-                   dialog.show();
+                    dialog = builder.create();
+                    dialog.show();
 
 
-                }else {
+                } else {
                     AlertDialog.Builder builder = new AlertDialog.Builder(MapaInspectorActivity.this);
                     builder.setTitle("Aun no has llegado")
-                            .setMessage("Por favor ir a "+marker.getPosition()+" como "+marker.getSnippet())
+                            .setMessage("Por favor ir a " + marker.getPosition() + " como " + marker.getSnippet())
                             .setPositiveButton("Ir", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
@@ -137,18 +168,17 @@ public class MapaInspectorActivity extends AppCompatActivity implements OnMapRea
     }
 
 
-
     ChildEventListener trampasHijoListener = new ChildEventListener() {
         @Override
         public void onChildAdded(DataSnapshot dataSnapshot, String s) {
 
             final Trampa trampa = dataSnapshot.getValue(Trampa.class);
 
-            if( mMap != null && trampa != null){
+            if (mMap != null && trampa != null) {
                 MarkerOptions markerOptions = new MarkerOptions()
-                        .title("Identificacion: "+trampa.id)
-                        .snippet("Indicio o Referencia: "+trampa.direccion)
-                        .position( new LatLng(trampa.latitud, trampa.longitud) );
+                        .title("Identificacion: " + trampa.id)
+                        .snippet("Indicio o Referencia: " + trampa.direccion)
+                        .position(new LatLng(trampa.latitud, trampa.longitud));
                 mMap.addMarker(markerOptions);
             }
 
@@ -175,94 +205,30 @@ public class MapaInspectorActivity extends AppCompatActivity implements OnMapRea
 
         }
     };
-    public static double calcularDistancia(LatLng a, LatLng b){
-        if(a != null && b != null){
+
+    public static double calcularDistancia(LatLng a, LatLng b) {
+        if (a != null && b != null) {
             // pasamos las diferencias de las coordenadas a radianes
-            double difLat = toRadianes( b.latitude - a.latitude );
-            double difLong = toRadianes( b.longitude - a.longitude );
+            double difLat = toRadianes(b.latitude - a.latitude);
+            double difLong = toRadianes(b.longitude - a.longitude);
 
-            double dato1 = Math.sin( difLat/2) * Math.sin(difLat/2) + Math.cos( toRadianes(a.latitude) ) *
-                    Math.cos( toRadianes(b.latitude) ) * Math.sin(difLong/2) * Math.sin(difLong/2);
+            double dato1 = Math.sin(difLat / 2) * Math.sin(difLat / 2) + Math.cos(toRadianes(a.latitude)) *
+                    Math.cos(toRadianes(b.latitude)) * Math.sin(difLong / 2) * Math.sin(difLong / 2);
 
-            double dato2 = 2 * Math.atan2( Math.sqrt(dato1), Math.sqrt(1 - dato1) );
+            double dato2 = 2 * Math.atan2(Math.sqrt(dato1), Math.sqrt(1 - dato1));
 
             double dato3 = RADIO_TIERRA * dato2;
 
-            Log.v(TAG, "Resutlado final (metros) "+dato3 * 1000);
+            Log.v(TAG, "Resutlado final (metros) " + dato3 * 1000);
 
             return dato3 * 1000;// lo pasamos a metros
         }
 
         return 0.0;// si a y b son nulos
     }
-    private static double toRadianes(double coord){
-        return ( (coord * Math.PI) / 180 );
-    }
-    protected void getLocation() {
-        if (isLocationEnabled(MapaInspectorActivity.this)) {
-            locationManager = (LocationManager)  this.getSystemService(Context.LOCATION_SERVICE);
-            criteria = new Criteria();
-            bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true)).toString();
 
-            //You can still do this if you like, you might get lucky:
-            Location location = locationManager.getLastKnownLocation(bestProvider);
-            if (location != null) {
-                Log.e("TAG", "GPS is on");
-                latitude = location.getLatitude();
-                longitude = location.getLongitude();
-                Toast.makeText(MainActivity.this, "latitude:" + latitude + " longitude:" + longitude, Toast.LENGTH_SHORT).show();
-                searchNearestPlace(voice2text);
-            }
-            else{
-                //This is what you need:
-                locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
-            }
-        }
-        else
-        {
-            //prompt user to enable location....
-            //.................
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        locationManager.removeUpdates(this);
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        //Hey, a non null location! Sweet!
-
-        //remove location callback:
-        locationManager.removeUpdates(this);
-
-        //open the map:
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
-        Toast.makeText(MainActivity.this, "latitude:" + latitude + " longitude:" + longitude, Toast.LENGTH_SHORT).show();
-        searchNearestPlace(voice2text);
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    public void searchNearestPlace(String v2txt) {
-        //.....
+    private static double toRadianes(double coord) {
+        return ((coord * Math.PI) / 180);
     }
 
 }
